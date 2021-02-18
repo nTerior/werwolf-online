5526e6d48e9b0907f49bced28e264a374f18bf7d
cephox <paul-stier@gmx.de>
2021-02-18 20:56:15 +0100

    Added server

---
+++ src/server/dev.ts
@@ -0,0 +1,15 @@
+import { EventEmitter } from "events";
+import { watch } from "fs";
+import { join } from "path";
+
+export var dev_events = new EventEmitter()
+
+export function devModeInit() {
+    watch(join(__dirname,"../../public/css"), (event,filename) => {
+        console.log(`Reloading css because of '${event} on ${filename}'`);
+        dev_events.emit("packet",{
+            css_reload: true
+        })
+        
+    })
+}
---
+++ src/server/websocket/handler.ts
@@ -0,0 +1,6 @@
+import { PacketResult } from "./server";
+import * as lws from "ws"
+
+export const packetHandler: {[key: string]: (data: any, ws: lws, id: string) => Promise<PacketResult>} = {
+    
+};

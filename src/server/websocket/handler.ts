43c387db156673a968a979ecadf45e1fcc4d1c03
cephox <paul-stier@gmx.de>
2021-02-18 21:12:43 +0100

    Full restart

--- src/server/websocket/handler.ts
+++
@@ -1,6 +0,0 @@
-import { PacketResult } from "./server";
-import * as lws from "ws"
-
-export const packetHandler: {[key: string]: (data: any, ws: lws, id: string) => Promise<PacketResult>} = {
-    
-};

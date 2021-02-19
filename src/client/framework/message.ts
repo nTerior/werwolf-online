69b66815711070c0abe2c29767854f8e598baef0
cephox <paul-stier@gmx.de>
2021-02-19 17:34:02 +0100

    Added messaging service

---
+++ src/client/framework/message.ts
@@ -0,0 +1,81 @@
+import { get_root } from "./framework"
+
+interface MessageBuild {
+    message: Message,
+    element: HTMLElement
+}
+
+export enum Urgency {
+    NORMAL = "urgency-normal",
+    WARNING = "urgency-warning",
+    ERROR = "urgency-error"
+}
+
+const message_stack: MessageBuild[] = []
+
+function pushMessage(message: Message) {
+    message_stack.forEach(m => {
+        var current = parseInt(m.element.style.bottom.replace(/[^0-9]/g, ""))
+        if(current.toString() == "NaN") current = 2
+        current += 5
+        m.element.style.bottom = current + "em"
+    })
+    
+    var build = buildMessage(message)
+    message_stack.push(build)
+    get_root().appendChild(build.element)
+    if(message.time > 0) setTimeout(() => removeMessage(build), message.time)
+}
+
+function removeMessage(message: MessageBuild) {
+    message.element.classList.remove("message-active")
+    message.element.classList.add("message-inactive")
+
+    var index = message_stack.findIndex(e => e === message)
+    message_stack.splice(index, 1)
+    message_stack.slice(0, index).forEach(m => {
+        var current = parseInt(m.element.style.bottom.replace(/[^0-9]/g, ""))
+        if(current.toString() == "NaN") current = 2
+        current -= 5
+        m.element.style.bottom = current + "em"
+    })
+
+    setTimeout(() => {
+        get_root().removeChild(message.element)
+    }, 1000)
+}
+
+function buildMessage(message: Message): MessageBuild {
+    var div = document.createElement("div")
+    div.classList.add("message", "message-active")
+    
+    var color = document.createElement("div")
+    color.classList.add("message-urgency", message.urgency)
+    div.appendChild(color)
+
+    var text = document.createElement("div")
+    text.classList.add("message-content")
+    text.textContent = message.text
+    div.appendChild(text)
+
+    return {
+        message: message,
+        element: div
+    }
+}
+
+export class Message {
+    public text: string
+    public urgency: Urgency
+    public time: number
+    
+    constructor(text: string, time: number = 5000, urgency: Urgency = Urgency.NORMAL) {
+        this.text = text
+        this.urgency = urgency
+        this.time = time
+    }
+
+    public display() {
+        pushMessage(this)
+    }
+}

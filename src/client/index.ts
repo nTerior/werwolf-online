fcd0245795f09f7ee5e4d964fdd6ec216a586404
cephox <paul-stier@gmx.de>
2021-02-19 10:23:11 +0100

    removed test packet

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,4 +1,3 @@
-import { Packet } from "../packet"
 import { State } from "./state"
 import { WebsocketClient } from "./websocket"
 
@@ -10,7 +9,6 @@ async function initWebsocket() {
     var ws = new WebsocketClient("ws://localhost:5354")
     await ws.start()
     State.ws = ws
-    State.ws.sendPacket(new Packet("asd"))
 }
 
 window.onload = init

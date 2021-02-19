e56ff082c9c0b8e1d6314d5a7632f447378651dc
cephox <paul-stier@gmx.de>
2021-02-19 11:43:23 +0100

    Added screen logic

--- src/client/framework/framework.ts
+++ src/client/framework/framework.ts
@@ -3,3 +3,7 @@ export const get_root = () => document.getElementById("webapp-root")!
 export function append(element: HTMLElement) {
     get_root().appendChild(element)
 }
+
+export function remove(element: HTMLElement) {
+    get_root().removeChild(element)
+}
---
+++ src/client/framework/screen/screen.ts
@@ -0,0 +1,61 @@
+import { append, remove } from "../framework"
+
+const main_title = "Werwölfe"
+
+export interface Screen {
+    element: HTMLElement,
+    title?: string,
+    on_push?: () => void,
+    on_pop?: () => void
+}
+
+export interface ScreenBuild {
+    screen: Screen,
+    element: HTMLElement
+}
+
+var screen_stack: ScreenBuild[] = []
+
+export function nextScreen(screen: Screen) {
+    popScreen()
+    pushScreen(screen)
+}
+
+export function pushScreen(screen: Screen) {
+    var build = buildScreen(screen)
+    if(screen.on_push) screen.on_push()
+    if(screen.title) document.title = main_title + " | " + screen.title
+    else document.title = main_title
+    screen_stack.push(build)
+    append(build.element)
+}
+
+export function popScreen() {
+    var build = screen_stack.pop()
+    if(!build) return
+
+    if(build.screen.on_pop) build.screen.on_pop()
+    build.element.classList.remove("screen-active")
+    build.element.classList.add("screen-inactive")
+
+    remove(build.element)
+
+    var next = screen_stack.pop()
+    if(next) {
+        screen_stack.push(next)
+        if(next.screen.title) document.title = main_title + " | " + next.screen.title
+    } else {
+        document.title = main_title
+    }
+}
+
+export function buildScreen(screen: Screen): ScreenBuild {
+    var div = document.createElement("div")
+    div.classList.add("screen", "screen-active")
+    screen.element.classList.add("screen-content")
+    div.appendChild(screen.element)
+    return {
+        screen: screen,
+        element: div
+    }
+}

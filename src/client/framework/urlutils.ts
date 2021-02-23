0249c815f136c1554bce937b8726cd2233af6108
cephox <paul-stier@gmx.de>
2021-02-23 14:20:13 +0100

    Updated game id retreval

--- src/client/framework/urlutils.ts
+++ src/client/framework/urlutils.ts
@@ -1,4 +1,4 @@
 export function get_game_id(): string | undefined {
-    var i = window.location.href.substring(window.location.href.lastIndexOf('/') + 1)
-    return i != "" && i != "werwolf" ? i : undefined
+    var path = window.location.pathname.split("/")
+    return path[path.length - 1]
 }

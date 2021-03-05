1f767a2cf97d3151cc88cbb26455948d2a990de3
cephox <paul-stier@gmx.de>
2021-03-05 12:06:20 +0100

    Removed player div if leaved

--- src/client/framework/screen/impl/gamescreen.ts
+++ src/client/framework/screen/impl/gamescreen.ts
@@ -19,19 +19,21 @@ export function generateGameScreen(): Screen {
 
 function createUserList(): HTMLDivElement {
 
+    var div = document.createElement("div")
+    div.classList.add("game-user-list")
+
     State.ws.setOnPacket("player-left", async packet => {
         var index = State.game.players.findIndex(e => e.id == packet.data.id)
         new Message(State.game.players[index].name + " hat das Spiel verlassen").display()
         State.game.players.splice(index, 1)
+
+        div.removeChild(document.getElementById("game-player-" + packet.data.id)!)
 
         var tmp = State.game.self_is_owner
 
         State.game.self_is_owner = (await State.ws.sendAndRecvPacket(new Packet("is_owner", State.game.id))).data
         if(State.game.self_is_owner && !tmp) new Message("Du bist nun der Host", -1).display()
     })
-
-    var div = document.createElement("div")
-    div.classList.add("game-user-list")
 
     State.game.players.forEach(p => {
         div.appendChild(createUser(p))

ca18eea670908c66804f51d970a40f214800e801
cephox <paul-stier@gmx.de>
2021-05-06 17:57:30 +0200

    Removed build folder

--- build/dest/client/App.tsx
+++
@@ -1,7 +0,0 @@
-export const App = () => {
-  return (
-    <>
-      <h1>React Typescript Template</h1>
-    </>
-  )
-}
--- build/dest/client/index.tsx
+++
@@ -1,4 +0,0 @@
-import ReactDOM from "react-dom"
-import { App } from "./App"
-console.log("OK")
-ReactDOM.render(<App />, document.getElementById("root"))
